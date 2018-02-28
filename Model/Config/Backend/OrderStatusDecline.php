68d99801a482269bb382a04aa7f73f0272509d52
Yurii Torbyk <jora.yt@gmail.com>
2018-02-28 21:34:38 +0200

    KOUNT-32: Add configuration for displaying declined and on review orders

---
+++ Model/Config/Backend/OrderStatusDecline.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright (c) 2017 KOUNT, INC.
+ * See COPYING.txt for license details.
+ */
+namespace Swarming\Kount\Model\Config\Backend;
+
+class OrderStatusDecline extends \Magento\Framework\App\Config\Value
+{
+    /**
+     * @var \Magento\Sales\Model\ResourceModel\Order\Status
+     */
+    private $orderStatusResource;
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
+     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
+     * @param \Magento\Sales\Model\ResourceModel\Order\Status $orderStatusResource
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\Sales\Model\ResourceModel\Order\Status $orderStatusResource,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->orderStatusResource = $orderStatusResource;
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * @return $this
+     */
+    public function beforeSave()
+    {
+        $this->orderStatusResource->assignState('decline_kount', 'holded', 0, $this->getValue());
+
+        parent::beforeSave();
+        return $this;
+    }
+}
