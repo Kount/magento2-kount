f34c4a0ed0857ae7a6063dc3cdc19260e09b3f50
Jon Colas <jon.colas@swarmingtech.com>
2020-01-14 14:02:17 -0600

    KOUNT-70 Respond to ENS Request on receipt of events instead of after processing

--- Controller/Ens/Index.php
+++ Controller/Ens/Index.php
@@ -80,7 +80,9 @@ class Index extends Action implements \Magento\Framework\App\CsrfAwareActionInte
             }
 
             $xmlString = file_get_contents('php://input');
+            $this->respondOnReceiptOfEvents();
             $response = $this->ensManager->handleRequest($xmlString);
+            $this->logger->info($response);
         } catch (\Exception $e) {
             $this->logger->error($e->getMessage());
             $this->logger->critical($e);
@@ -110,6 +112,29 @@ class Index extends Action implements \Magento\Framework\App\CsrfAwareActionInte
         return $this->configAccount->isTestMode()
             ||
             $this->configEns->isAllowedIp($this->remoteAddress->getRemoteAddress());
+    }
+
+    /**
+     * Create response upon receipt of request instead of after processing.  The initial request can be added to a queue
+     * and processed via cron at a later date, for now, we will just respond upon receipt and keep processing going now.
+     * For now, we will send response now and and keep session alive to process data.
+     *
+     * @return void
+     */
+    protected function respondOnReceiptOfEvents()
+    {
+        ob_start();
+        echo "200 OK";
+        $size = ob_get_length();
+        header("Content-Encoding: none");
+        header("Content-Length: {$size}");
+        header("Connection: close");
+        ob_end_flush();
+        ob_flush();
+        flush();
+        if (session_id()) {
+            session_write_close();
+        }
     }
 
     /**
